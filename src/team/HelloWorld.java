954ca56df64ed0c85c79c72324b1f22a015b8d3f
qqyan <qqyan@192.168.30.150>
2023-11-21 16:41:55 -0700

    2nd Commit

--- src/team/HelloWorld.java
+++ src/team/HelloWorld.java
@@ -3,6 +3,7 @@ package team;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		 System.out.println("Hello World!");
 		 System.out.println("Hello World!");// TODO Auto-generated method stub
 
 	}
