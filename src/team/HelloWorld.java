2b9ba5436b791d9b322bde250b6e70cdd13ea2a1
QQJoJolinlin <151688857+QQJoJolinlin@users.noreply.github.com>
2023-11-21 17:23:32 -0700

    Update HelloWorld.java

--- src/team/HelloWorld.java
+++ src/team/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		 System.out.println("Hello World!");
+		 System.out.println("Hello World!");
 		 System.out.println("Hello World!");// TODO Auto-generated method stub
 
 	}
